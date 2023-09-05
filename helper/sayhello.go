f590b97c35cda4ac12e68c7903d2c4c609a5c99d
Fauzi <73748420+fauzibrillian@users.noreply.github.com>
2023-09-05 14:49:35 +0700

    golang

---
+++ helper/sayhello.go
@@ -0,0 +1,13 @@
+package helper
+
+import "fmt"
+
+var version
+
+func Hello(name string) {
+	fmt.Println("Hellow jancog", name)
+}
+
+func goodbye(name string) {
+	fmt.Println("Goodbye")
+}
---
+++ import.go
@@ -0,0 +1,8 @@
+package main
+
+import "belajar_go/helper"
+
+func main() {
+	helper.Hello("fauzi")
+	helper.goodbye("fauzi")
+}
